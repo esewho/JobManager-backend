26edb546af529a81c8d383cafdfda74d549b95e9
esewho <albertogfcb@gmail.com>
2026-01-29 22:30:30 +0100

    reinstalled versions

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig, env } from 'prisma/config';
+import 'dotenv/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -7,4 +7,4 @@ export default defineConfig({
   datasource: {
     url: env('DATABASE_URL'),
   },
-})
+});
